110aa2495425f451100a74d02b5120fbbbd6b492
Michal-s77 <michalslazak00@gmail.com>
2025-07-05 22:56:51 +0200

    Zadanie domowe 4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Michał";
+char gender = 'M';
+var age = 38;
+
+if (gender == 'K' && (age < 30))
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+ if (name == "Ewa" && (age == 30))
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+ if (gender == 'M' && (age < 18)) 
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
